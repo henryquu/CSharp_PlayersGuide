b664128e3ac705e283872fdda338a07bdc7046cc
henryquu <henryquu@gmail.com>
2023-07-25 23:17:54 +0200

    The Door

--- TheCard/Program.cs
+++ TheCard/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+Color[] colors = new Color[] { Color.Red, Color.Green, Color.Blue, Color.Yellow };
+Rank[] ranks = new Rank[] { 
+    Rank.One, Rank.Two, 
+    Rank.Three, Rank.Four, 
+    Rank.Five, Rank.Six, 
+    Rank.Seven, Rank.Eight, 
+    Rank.Nine, Rank.Ten, 
+    Rank.DollarSign, Rank.Percent, 
+    Rank.Caret, Rank.Ampersand 
+};
+
+Card a;
+
+foreach (Rank rank in ranks) {
+    foreach (Color color in colors) {
+        a = new Card(rank, color);
+        Console.WriteLine($"The {a.Color} {a.Rank}");
+    }
+}
+
+
+class Card {
+    public Color Color { get; }
+    public Rank Rank { get; }
+    
+    public bool isSymbol() {
+        return Rank == Rank.DollarSign | Rank == Rank.Percent | Rank == Rank.Caret | Rank == Rank.Ampersand;
+    }
+
+    public bool isNumber() {
+        return !isSymbol();
+    }
+
+    public Card(Rank rank, Color color) {
+        Color = color;
+        Rank = rank;
+    }
+}
+
+enum Color { Red, Green, Blue, Yellow }
+enum Rank { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, DollarSign, Percent, Caret, Ampersand }
---
+++ TheLockedDoor/Program.cs
@@ -0,0 +1,86 @@
+﻿Door aa = new Door(2543);
+Console.WriteLine(aa.State);
+aa.Close();
+aa.Open();
+aa.Close();
+aa.Lock();
+aa.ChangePasscode();
+aa.Unlock();
+
+
+class Door {
+    public LockState State { get; private set; }
+    private int Passcode;
+
+    public Door(int passcode) {
+        Passcode = passcode;
+        State = LockState.Open;
+    }
+
+    public bool Close() {
+        if (State == LockState.Open){
+            State = LockState.Closed;
+            Console.WriteLine("Closed the door!");
+            return true;
+        }
+        Console.WriteLine("Door is not open!");
+        return false;
+    }
+
+    public bool Unlock() {
+        if (State == LockState.Locked) {
+            Console.Write("Enter the passcode: ");
+            int passcode = Convert.ToInt32(Console.ReadLine());
+            
+            if (Passcode == passcode) {
+                State = LockState.Closed;
+                Console.WriteLine("Valid passcode, door ist now closed!");
+                return true;
+            }
+            else {
+                Console.WriteLine("The passcode is invalid!");
+                return false;
+            }
+        }
+        Console.WriteLine("Door is not locked!");
+        return false;
+    }
+
+    public bool Open() {
+        if (State == LockState.Open) {
+            State = LockState.Closed;
+            Console.WriteLine("Opened the door!");
+            return true;
+        }
+        Console.WriteLine("Door needs to be unlocked!");
+        return false;
+    }
+
+    public bool Lock() {
+        if (State == LockState.Closed) {
+            State = LockState.Locked;
+            Console.WriteLine("Locked the door!");
+            return true;
+        }
+        Console.WriteLine("Door needs to be unlocked!");
+        return false;
+    }
+
+    public bool ChangePasscode() {
+        Console.Write("Enter the current passcode: ");
+        int passcode = Convert.ToInt32(Console.ReadLine());
+
+        if (Passcode == passcode) {
+            Console.Write("Enter the new passcode: ");
+            Passcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Passcode changed succesfully!");
+            return true;
+        }
+
+        Console.WriteLine("Wrong passcode.");
+        return false;
+    }
+}
+
+enum LockState { Open, Closed, Locked };
