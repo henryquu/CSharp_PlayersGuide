4d44e0ca907ebdb4c1d03828004bb63d4f50bbd1
henryquu <henryquu@gmail.com>
2023-08-31 23:39:19 +0200

    coloreditems

---
+++ ColoredItems/Program.cs
@@ -0,0 +1,29 @@
+﻿ColoredItem<Sword> sword = new ColoredItem<Sword>(ConsoleColor.Blue, 10);
+ColoredItem<Bow> bow = new ColoredItem<Bow>(ConsoleColor.Red, 10);
+ColoredItem<Axe> axe = new ColoredItem<Axe>(ConsoleColor.Green, 10);
+
+sword.Display();
+bow.Display();
+axe.Display();
+
+
+public class ColoredItem<T> {
+    int weight;
+    ConsoleColor color;
+
+    public ColoredItem(ConsoleColor Color, int Weight){
+        weight = Weight;
+        color = Color;
+    }
+    public void Display() {
+        Console.ForegroundColor = color;
+        Console.WriteLine(this.ToString());
+    }
+}
+
+public class Sword {
+}
+public class Bow {
+}
+public class Axe {
+}
