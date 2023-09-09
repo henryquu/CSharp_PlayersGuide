f45ffdb4b2d25e9bf56215e5df7e092e6cc63758
henryquu <henryquu@gmail.com>
2023-09-09 10:08:42 +0200

    Safe nnumber crunching

--- DuelingTraditions/Program.cs
+++ DuelingTraditions/Program.cs
@@ -1,24 +1,5 @@
 ﻿internal partial class Program {
     static void Main(string[] args) {
-        new Game(new RobotPilot());
-
-    public interface IPilot {
-        public int GetPosition();
+        new Game.Game(new Pilot.RobotPilot());
     }
-
-    public class RobotPilot:IPilot {
-        public int GetPosition() {
-            Random random = new Random();
-
-            Console.Clear();
-            return random.Next(1, 100);
-        }
-    }
-
-    public class HumanPilot:IPilot {
-        public int GetPosition() {
-            return Player.GetGuess("Enter the position of the Manticore: ");
-        }
-    }
-}
 }
---
+++ SaferNumberCrunching/Program.cs
@@ -0,0 +1,20 @@
+﻿int integer;
+do {
+    Console.WriteLine("Enter an integer");
+  
+} while (!int.TryParse(Console.ReadLine(), out integer));
+Console.WriteLine(integer);
+
+double doub;
+do {
+    Console.WriteLine("Enter an double");
+
+} while (!double.TryParse(Console.ReadLine(), out doub));
+Console.WriteLine(doub);
+
+float floating;
+do {
+    Console.WriteLine("Enter an float");
+
+} while (!float.TryParse(Console.ReadLine(), out floating));
+Console.WriteLine(floating);
