9cff85d990f346d2a6cc1ba7ead205eb20083412
henryquu <henryquu@gmail.com>
2023-09-09 11:29:30 +0200

    Better random

---
+++ BetterRandom/Program.cs
@@ -0,0 +1,18 @@
+﻿public static class RandomExtension {
+    public static double NextDouble(this Random rnd, double max) {
+        return rnd.NextDouble() * max;
+    }
+
+    public static string NextString(this Random rnd, params string[] strings) {
+        return strings[rnd.Next(strings.Length)];
+    }
+
+    public static bool CoinFlip(this Random rnd, double heads=0.5) {
+        if (heads < 0 || heads > 1) 
+            return false;
+
+        if (rnd.NextDouble() < heads) 
+            return true;
+        return false;
+    }
+}
